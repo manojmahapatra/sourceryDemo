872e754d9418b4c023e0bc2f06bbe2fd6a853d14
Manoj Mahapatra <manojkumar.mahapatra@walmartlabs.com>
2019-10-23 19:46:30 -0700

    Initial Commit

---
+++ sourceryDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  sourceryDemo
+//
+//  Created by Manoj Kumar Mahapatra on 10/23/19.
+//  Copyright © 2019 Manoj Kumar Mahapatra. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ sourceryDemoTests/sourceryDemoTests.swift
@@ -0,0 +1,34 @@
+//
+//  sourceryDemoTests.swift
+//  sourceryDemoTests
+//
+//  Created by Manoj Kumar Mahapatra on 10/23/19.
+//  Copyright © 2019 Manoj Kumar Mahapatra. All rights reserved.
+//
+
+import XCTest
+@testable import sourceryDemo
+
+class sourceryDemoTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
